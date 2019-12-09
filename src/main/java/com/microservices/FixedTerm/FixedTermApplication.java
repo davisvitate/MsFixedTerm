b47accd59f1ff35ae842d2f1b6fc11f921c9e4f5
davisvitate <davisvitate@gmail.com>
2019-12-09 15:39:13 -0500

    primera version

---
+++ src/main/java/com/microservices/FixedTerm/FixedTermApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.FixedTerm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedTermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedTermApplication.class, args);
+	}
+
+}
